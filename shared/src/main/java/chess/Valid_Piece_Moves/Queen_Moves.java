4ae53517020546ea1b61ec7fb6f5f35bfc95dc56
CelestialOkamii <danielle.ebenal@outlook.com>
2025-09-19 21:25:31 -0600

    Queen_Moves: was having a hard time debugging so I'm trying to use a more direct approach by moving in the same direction by using addition until a piece is reached then breaking and moving onto the next direction. Still getting index -1 out of bounds for length 8 though in the autograder

--- shared/src/main/java/chess/Valid_Piece_Moves/Queen_Moves.java
+++ shared/src/main/java/chess/Valid_Piece_Moves/Queen_Moves.java
@@ -18,177 +18,132 @@ public class Queen_Moves {
 
     public List<ChessMove> valid_moves() {
         List<ChessMove> moves = new ArrayList<>();
-        int current_row = position.getRow();
-        int current_column = position.getColumn();
-        List<ChessPosition> moves_to_try = valid_positions();
-        List<ChessPosition> possible_positions = new ArrayList<>();
-        List<ChessPosition> no_go_past = new ArrayList<>();
-        for (ChessPosition item : moves_to_try) {
-            int row = item.getRow();
-            int column = item.getColumn();
-            List<ChessPosition> valid_boundaries = valid_move(row, column, no_go_past);
-            //if piece is same color as Queen will return position of [1,1]
-            //if opposing color will return position of [0,0] for valid_boundaries.get(1)
-            //if there is no piece will return position [2,2]
-            //if position isn't valid will return [-1,-1] for valid_boundaries.get(0)
-            int valid_row = valid_boundaries.get(0).getRow();
-            int piece_type = valid_boundaries.get(1).getRow();
-            if (valid_row > 0 && valid_row < 7 && piece_type != 1) {
-                ChessMove valid = new ChessMove(position, valid_boundaries.get(0), null);
-                moves.add(valid);
-            }
-            if (piece_type == 1 || piece_type == 0) {
-                if (current_row >= row && current_column != column) {
-                    Iterator<ChessPosition> it = possible_positions.iterator();
-                    while (it.hasNext()) {
-                        ChessPosition pos = it.next();
-                        if (piece_type == 1) {
-                            if (pos.getColumn() < column || pos.getColumn() > column) {
-                                it.remove();
-                            }
-                        }
-                        else {
-                            if (pos.getColumn() <= column || pos.getColumn() >= column) {
-                                it.remove();
-                            }
-                        }
-                    }
-                }
-                else if (current_row == row) {
-                    if (column < current_column) {
-                        Iterator<ChessPosition> it = possible_positions.iterator();
-                        while (it.hasNext()) {
-                            ChessPosition pos = it.next();
-                            if (piece_type == 1 && pos.getRow() == current_row) {
-                                if (pos.getColumn() < column) {
-                                    it.remove();
-                                }
-                            }
-                            else {
-                                if (pos.getColumn() <= column) {
-                                    it.remove();
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (current_column == column) {
-                    if (row < current_row) {
-                        Iterator<ChessPosition> it = possible_positions.iterator();
-                        while (it.hasNext()) {
-                            ChessPosition pos = it.next();
-                            if (piece_type == 1 && pos.getColumn() == current_column) {
-                                if (pos.getRow() < row) {
-                                    it.remove();
-                                }
-                            }
-                            else {
-                                if (pos.getRow() <= row) {
-                                    it.remove();
-                                }
-                            }
-                        }
-                    }
-                }
-                else {
-                    no_go_past.add(valid_boundaries.get(0));
-                }
-            }
+        List<ChessPosition> valid_moves = valid_positions();
+        for (ChessPosition move : valid_moves) {
+            ChessMove pos_move = new ChessMove(position, move, null);
+            moves.add(pos_move);
         }
         return moves;
     }
 
-    private List<ChessPosition> valid_move(int row, int column, List<ChessPosition> no_go_past) {
-        int current_row = position.getRow();
-        int current_column = position.getColumn();
-        List<ChessPosition> move_piece_color = new ArrayList<>();
-        ChessPosition pos = new ChessPosition(row,column);
-        for (ChessPosition bad_spot : no_go_past) {
-            int bad_row = bad_spot.getRow();
-            int bad_column = bad_spot.getColumn();
-            ChessPosition bad_move = new ChessPosition(-1,-1);
-            if ((row > bad_row && row != current_row) && (column != current_column && (column > bad_column  && column > current_column|| column < bad_column && column < current_column))) {
-                move_piece_color.add(bad_move);
-                if (board.getPiece(pos) != null) {
-                    ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
-                    if (color == board.getPiece(position).getTeamColor()) {
-                        ChessPosition piece = new ChessPosition(1,1);
-                        move_piece_color.add(piece);
-                        return move_piece_color;
-                    }
-                    else {
-                        ChessPosition piece = new ChessPosition(0,0);
-                        move_piece_color.add(piece);
-                        return move_piece_color;
-                    }
-                }
-                else {
-                    ChessPosition empty = new ChessPosition(2,2);
-                    move_piece_color.add(empty);
-                    return move_piece_color;
-                }
-            }
-            else if ((bad_row == current_row && bad_column < column) || (bad_column == current_column && row > bad_row)) {
-                move_piece_color.add(bad_move);
-                move_piece_color.add(bad_move);
-                return move_piece_color;
-            }
-            move_piece_color.add(pos);
-            if (board.getPiece(pos) != null) {
-                ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
-                if (color == board.getPiece(position).getTeamColor()) {
-                    ChessPosition piece = new ChessPosition(1,1);
-                    move_piece_color.add(piece);
-                    return move_piece_color;
-                }
-                else {
-                    ChessPosition piece = new ChessPosition(0,0);
-                    move_piece_color.add(piece);
-                    return move_piece_color;
-                }
-            }
-            else {
-                ChessPosition empty = new ChessPosition(2,2);
-                move_piece_color.add(empty);
-                return move_piece_color;
-            }
-        }
-        return move_piece_color;
-    }
-
     private List<ChessPosition> valid_positions() {
+        int[][] paths = {{0,1}, {0,-1}, {-1,0}, {1,0}, {1,-1}, {1,1}, {-1,-1}, {-1,1}};
         List<ChessPosition> positions = new ArrayList<>();
-        int column = position.getColumn();
-        for (int i = position.getRow() + 1; i < 8; i++) {
-            if (i + 1 < 8) {
-                ChessPosition pos = new ChessPosition(i, i + 1);
-                positions.add(pos);
+        for (int[] course : paths) {
+            int row = position.getRow();
+            int column = position.getColumn();
+            while (true) {
+                row = row + course[0];
+                column = column + course[1];
+                if (row < 0 || row > 7 || column < 0 || column > 7) {
+                    break;
+                }
+                ChessPosition pos = new ChessPosition(row, column);
+                if (board.getPiece(pos) == null) {
+                    positions.add(pos);
+                }
+                else {
+                    if (board.getPiece(pos).getTeamColor() == board.getPiece(position).getTeamColor()) {
+                        positions.add(pos);
+                    }
+                    break;
+                }
             }
-            if (column - 1 > -1) {
-                ChessPosition pos = new ChessPosition(i, i - 1);
-                positions.add(pos);
-                column--;
-            }
-        }
-        column = position.getColumn();
-        for (int i = position.getRow() - 1; i > -1; i--) {
-            if (column + 1 < 8) {
-                ChessPosition pos = new ChessPosition(i, i + 1);
-                positions.add(pos);
-            }
-            if (column - 1 > -1) {
-                ChessPosition pos = new ChessPosition(i, i - 1);
-                positions.add(pos);
-            }
-        }
-        for (int i = 0; i < 8; i++) {
-            ChessPosition col = new ChessPosition(i, position.getColumn());
-            positions.add(col);
-            ChessPosition row = new ChessPosition(position.getRow(), i);
-            positions.add(row);
         }
         return positions;
     }
+
+//    private List<Conditions> valid_move(int row, int column, List<ChessPosition> no_go_past) {
+//        int current_row = position.getRow();
+//        int current_column = position.getColumn();
+//        List<Conditions> move_piece_color = new ArrayList<>();
+//        ChessPosition pos = new ChessPosition(row,column);
+//        if (no_go_past.size() > 0) {
+//            for (ChessPosition bad_spot : no_go_past) {
+//                int bad_row = bad_spot.getRow();
+//                int bad_column = bad_spot.getColumn();
+//                if ((row > bad_row && row != current_row) && (column != current_column && (column > bad_column  && column > current_column|| column < bad_column && column < current_column))) {
+//                    move_piece_color.add(Conditions.INVALID);
+//                    if (board.getPiece(pos) != null) {
+//                        ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
+//                        if (color == board.getPiece(position).getTeamColor()) {
+//                            move_piece_color.add(Conditions.SAME_COLOR);
+//                            return move_piece_color;
+//                        }
+//                        else {
+//                            move_piece_color.add(Conditions.DIFF_COLOR);
+//                            return move_piece_color;
+//                        }
+//                    }
+//                    else {
+//                        move_piece_color.add(Conditions.EMPTY);
+//                        return move_piece_color;
+//                    }
+//                }
+//                else if ((bad_row == current_row && bad_column < column) || (bad_column == current_column && row > bad_row)) {
+//                    move_piece_color.add(Conditions.INVALID);
+//                    move_piece_color.add(Conditions.EMPTY);
+//                    return move_piece_color;
+//                }
+//            }
+//        }
+//        move_piece_color.add(Conditions.VALID);
+//        if (board.getPiece(pos) != null) {
+//            ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
+//            if (color == board.getPiece(position).getTeamColor()) {
+//                move_piece_color.add(Conditions.SAME_COLOR);
+//                return move_piece_color;
+//            }
+//            else {
+//                move_piece_color.add(Conditions.DIFF_COLOR);
+//                return move_piece_color;
+//            }
+//        }
+//        else {
+//            move_piece_color.add(Conditions.EMPTY);
+//            return move_piece_color;
+//        }
+//    }
+//
+//    private List<ChessPosition> valid_positions() {
+//        List<ChessPosition> positions = new ArrayList<>();
+//        int curr_col = position.getColumn();
+//        int curr_row = position.getRow();
+//        for (int i = curr_row + 1; i < 8; i++) {
+//            int column_down = i - curr_row;
+//            if (curr_col + column_down < 8) {
+//                ChessPosition pos = new ChessPosition(i, curr_col + column_down);
+//                positions.add(pos);
+//            }
+//            if (curr_col - column_down > -1) {
+//                ChessPosition pos = new ChessPosition(i, curr_col - column_down);
+//                positions.add(pos);
+//            }
+//        }
+//        for (int i = curr_row - 1; i > -1; i--) {
+//            int column_up = curr_row - i;
+//            if (curr_col + column_up < 8) {
+//                ChessPosition pos = new ChessPosition(i, curr_col + column_up);
+//                positions.add(pos);
+//            }
+//            if (curr_col - column_up > -1) {
+//                ChessPosition pos = new ChessPosition(i, curr_col - column_up);
+//                positions.add(pos);
+//            }
+//        }
+//        for (int i = 0; i < 8; i++) {
+//            if (i != curr_row) {
+//                ChessPosition col = new ChessPosition(i, curr_col);
+//                positions.add(col);
+//                ChessPosition row = new ChessPosition(curr_row, i);
+//                positions.add(row);
+//            }
+//            else {
+//
+//            }
+//        }
+//        return positions;
+//    }
 
 
     @Override
