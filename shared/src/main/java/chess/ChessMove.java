6a5de2c064e4ba91d936cbc8ff5e7351058504ef
CelestialOkamii <danielle.ebenal@outlook.com>
2026-01-26 15:12:17 -0700

    - Copied old ChessMove to here, check my Old_Chess repository to see my commits

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,4 +1,5 @@
 package chess;
+import java.util.*;
 
 /**
  * Represents moving a chess piece on a chessboard
@@ -8,22 +9,29 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -33,6 +41,36 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Start Position: %s, End Position: %s", startPosition, endPosition);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        ChessMove pos = (ChessMove) obj;
+        boolean startAndEnd = Objects.equals(startPosition, pos.startPosition) && Objects.equals(endPosition, pos.endPosition);
+        return startAndEnd && Objects.equals(promotionPiece, pos.promotionPiece);
+    }
+
+    @Override
+    public int hashCode() {
+        int startInt = startPosition.getRow() * 8 + startPosition.getColumn();
+        int endInt   = endPosition.getRow() * 8 + endPosition.getColumn();
+        int promoInt = (promotionPiece != null) ? promotionPiece.ordinal() : -1;
+        int hash = 17;
+        hash = 31 * hash + startInt;
+        hash = 31 * hash + endInt;
+        hash = 31 * hash + promoInt;
+        return hash;
     }
 }
