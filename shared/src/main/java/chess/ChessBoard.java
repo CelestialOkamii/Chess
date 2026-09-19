eb38d61f67356fd955bea09b65accca6db092922
CelestialOkamii <danielle.ebenal@outlook.com>
2026-09-18 21:02:40 -0600

    Hopefully got methods for ChessBoard Done

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.*;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,9 +10,15 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] board = new ChessPiece[8][8];
+    private Map<ChessPosition, ChessPiece> whitePiecePos = new HashMap<>();
+    private Map<ChessPosition, ChessPiece> blackPiecePos = new HashMap<>();
+
+
     public ChessBoard() {
         
     }
+
 
     /**
      * Adds a chess piece to the chessboard
@@ -19,8 +27,9 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
+
 
     /**
      * Gets a chess piece on the chessboard
@@ -30,14 +39,75 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow() - 1][position.getColumn() - 1];
     }
+
 
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        List<Integer> startRows = new ArrayList<>(Arrays.asList(1, 2, 7, 8));
+        for (int row : startRows) {
+            List<ChessPosition> positions = startRows(row);
+            List<ChessPiece> pieces = rowPieces(row);
+            for (ChessPosition position : positions) {
+                addPiece(position, pieces.get(positions.indexOf(position)));
+                if (row == 1 || row == 2) {
+                    whitePiecePos.put(position, pieces.get(positions.indexOf(position)));
+                }
+                else {
+                    blackPiecePos.put(position, pieces.get(positions.indexOf(position)));
+                }
+            }
+        }
+
+    }
+
+
+    private List<ChessPosition> startRows(int row) {
+        List<ChessPosition> positions = new ArrayList<>();
+        for (int col = 1 ; col < 9; col++) {
+            ChessPosition newPos = new ChessPosition(row, col);
+            positions.add(newPos);
+        }
+        return positions;
+    }
+
+
+    private List<ChessPiece> rowPieces(int row) {
+        List<ChessPiece> endRow = new ArrayList<>();
+        List<ChessPiece.PieceType> pawns = new ArrayList<>(Arrays.asList(
+                ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN, ChessPiece.PieceType.PAWN
+        ));
+        List<ChessPiece.PieceType> specialized = new ArrayList<>(Arrays.asList(
+                ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KING, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.ROOK
+        ));
+        if (row == 1) {
+            for (ChessPiece.PieceType type : specialized) {
+                ChessPiece piece = new ChessPiece(ChessGame.TeamColor.WHITE, type);
+                endRow.add(piece);
+            }
+        }
+        else if (row == 2) {
+            for (ChessPiece.PieceType type : pawns) {
+                ChessPiece piece = new ChessPiece(ChessGame.TeamColor.WHITE, type);
+                endRow.add(piece);
+            }
+        }
+        else if (row == 7) {
+            for (ChessPiece.PieceType type : pawns) {
+                ChessPiece piece = new ChessPiece(ChessGame.TeamColor.BLACK, type);
+                endRow.add(piece);
+            }
+        }
+        else {
+            for (ChessPiece.PieceType type : specialized) {
+                ChessPiece piece = new ChessPiece(ChessGame.TeamColor.BLACK, type);
+                endRow.add(piece);
+            }
+        }
+        return endRow;
     }
 }
